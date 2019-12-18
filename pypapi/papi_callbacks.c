2188acab38a28af0a70a3bd2f36060fa9984e096
Marcin Copik <mcopik@gmail.com>
2019-12-18 18:24:58 +0100

    Add debug printout

--- pypapi/papi_callbacks.c
+++ pypapi/papi_callbacks.c
@@ -5,6 +5,9 @@
 
 #include <papi.h>
 
+#define DEBUG 0
+#define DEBUG_PRINT(fmt, ...)\
+  do { if(DEBUG) fprintf(stderr, "PYPAPI: "fmt, __VA_ARGS__); } while(0)
 
 #define BUFFER_SIZE 100
 
@@ -23,9 +26,11 @@ long long * overflow_buffer(int size, int event_count)
       buffer = calloc(BUFFER_SIZE, sizeof(long long*));
       buffer_size = BUFFER_SIZE;
       buffer_counter = 0;
+      DEBUG_PRINT("Allocated buffers storage of length %d\n", BUFFER_SIZE);
 
       values = malloc(sizeof(long long) * size * (event_count + 1));
       buffer[buffer_counter++] = values;
+      DEBUG_PRINT("Allocate storage %p of size %ld at pos %d\n", values, sizeof(long long) * size * (event_count + 1), buffer_counter - 1);
       events = event_count;
       cur_buffer_size = size;
     }
@@ -46,9 +51,11 @@ long long * overflow_buffer(int size, int event_count)
       if(buffer_counter == buffer_size) {
         buffer_size *= 2;
         buffer = realloc(buffer, sizeof(long long*) * buffer_size);
+        DEBUG_PRINT("Reallocate buffers storage to length %d\n", buffer_size);
       }
-      buffer[buffer_counter++] = values;
       values = calloc(cur_buffer_size * (events + 1), sizeof(long long));
+      buffer[buffer_counter++] = values;
+      DEBUG_PRINT("Allocate storage %p of size %ld at pos %d\n", values, cur_buffer_size * (events + 1) * sizeof(long long), buffer_counter - 1);
       counter = 0;
     }
     return &values[(events + 1) * counter++];
@@ -67,6 +74,7 @@ int overflow_buffer_size(int cnt)
 
 long long * overflow_buffer_access(int cnt)
 {
+  DEBUG_PRINT("Overflow buffer acccess %p at %d\n", buffer[cnt], cnt);
   return buffer[cnt];
 }
 
@@ -96,6 +104,7 @@ void overflow_C_callback(
   long long cur_time = tp.tv_sec * 1000 * 1000 + tp.tv_usec;
   long long * data = overflow_buffer_get();
   data[0] = cur_time;
+  DEBUG_PRINT("Overflow storage at %p\n", data);
   PAPI_read(EventSet, data + 1);
 }
 
